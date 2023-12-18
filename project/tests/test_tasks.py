f37feae9f66facff47c895534d61c8aa5340b199
ChingT <j60626j@gmail.com>
2023-12-18 22:54:50 +0100

    Add test_task

--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -1,3 +1,12 @@
+from worker import create_task
+
+
+def test_task():
+    assert create_task.run(1)
+    assert create_task.run(2)
+    assert create_task.run(3)
+
+
 def test_home(test_app):
     response = test_app.get("/")
     assert response.status_code == 200
