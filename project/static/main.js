04828bf682c088cd660e183a201852e095d108ba
ChingT <j60626j@gmail.com>
2023-12-18 23:10:33 +0100

    Revert changes in main.js

--- project/static/main.js
+++ project/static/main.js
@@ -13,7 +13,9 @@ function handleClick(type) {
     body: JSON.stringify({ type: type }),
   })
     .then((response) => response.json())
-    .then((res) => getStatus(res.data.task_id));
+    .then((res) => {
+      getStatus(res.task_id);
+    });
 }
 
 function getStatus(taskID) {
@@ -25,19 +27,20 @@ function getStatus(taskID) {
   })
     .then((response) => response.json())
     .then((res) => {
+      console.log(res);
       const html = `
       <tr>
         <td>${taskID}</td>
-        <td>${res.data.task_status}</td>
-        <td>${res.data.task_result}</td>
+        <td>${res.task_status}</td>
+        <td>${res.task_result}</td>
       </tr>`;
-      document.getElementById("tasks").prepend(html);
-      const newRow = document.getElementById("table").insertRow();
+      const newRow = document.getElementById("tasks").insertRow(0);
       newRow.innerHTML = html;
-      const taskStatus = res.data.task_status;
-      if (taskStatus === "finished" || taskStatus === "failed") return false;
+
+      const taskStatus = res.task_status;
+      if (taskStatus === "SUCCESS" || taskStatus === "FAILURE") return false;
       setTimeout(function () {
-        getStatus(res.data.task_id);
+        getStatus(res.task_id);
       }, 1000);
     })
     .catch((err) => console.log(err));
