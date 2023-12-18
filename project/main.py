6bb8ee00ce203b7096551d93ca4cfe2f094996cb
ChingT <j60626j@gmail.com>
2023-12-18 22:39:19 +0100

    Update get_status route handler to return the task status and result

--- project/main.py
+++ project/main.py
@@ -1,3 +1,4 @@
+from celery.result import AsyncResult
 from fastapi import Body, FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
@@ -23,4 +24,10 @@ def run_task(payload=Body(...)):
 
 @app.get("/tasks/{task_id}")
 def get_status(task_id):
-    return JSONResponse(task_id)
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result,
+    }
+    return JSONResponse(result)
