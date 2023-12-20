f8d5c00bcac23c2d558a910998fdd4c14ca46992
ChingT <j60626j@gmail.com>
2023-12-20 13:02:03 +0100

    Add tasks

--- project/main.py
+++ project/main.py
@@ -1,16 +1,23 @@
 import logging
 
+from celery import Celery
 from celery.result import AsyncResult
 from celery.signals import after_setup_logger
+from config import settings
 from fastapi import Body, FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from worker import create_task
+from worker import create_task, generate_transaction_report, send_notification
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+
+celery = Celery(
+    __name__, broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_RESULT_BACKEND
+)
 
 
 logging.basicConfig(
@@ -32,7 +39,7 @@ def setup_celery_logger(logger, *args, **kwargs):
     logger.addHandler(fh)
 
 
-@app.get("/")
+@app.get("/home")
 def home(request: Request):
     return templates.TemplateResponse("home.html", context={"request": request})
 
@@ -53,3 +60,16 @@ def get_status(task_id):
         "task_result": task_result.result,
     }
     return JSONResponse(result)
+
+
+@app.get("/push/{device_token}")
+async def notify(device_token: str):
+    logger.info("sending notification in background")
+    send_notification.delay(device_token)
+    return JSONResponse({"message": "Notification sent"})
+
+
+@app.get("/export/report")
+async def export():
+    generate_transaction_report.delay()
+    return JSONResponse({"message": "Generating report"})
--- project/worker.py
+++ project/worker.py
@@ -1,16 +1,39 @@
-import os
+import random
 import time
 
-from celery import Celery
+from celery import shared_task
+from celery.utils.log import get_task_logger
 
-celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get(
-    "CELERY_RESULT_BACKEND", "redis://localhost:6379"
-)
+logger = get_task_logger("tasks")
 
 
-@celery.task(name="create_task")
+@shared_task
 def create_task(task_type):
     time.sleep(int(task_type) * 10)
+    return True
+
+
+@shared_task
+def push_error_task(device_token: str):
+    logger.info("starting background task")
+    time.sleep(2)
+    11 / 0
+    logger.info(f"notification sent {device_token}")
+
+
+@shared_task(bind=True, queue="high_priority")
+def send_notification(self, device_token: str):
+    try:
+        logger.info("starting background task")
+        time.sleep(1)
+        if random.choice([0, 1]):
+            raise Exception()
+    except Exception as e:
+        raise self.retry(exc=e, countdown=3, max_retries=3)
+    logger.info(f"notification sent {device_token}")
+
+
+@shared_task
+def generate_transaction_report():
+    time.sleep(5)
     return True
